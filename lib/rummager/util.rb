6a9f50b8cd31cf7895581308e260d467be561ada
Theodore Vaida <ted@kstechnologies.com>
2014-12-22 19:09:55 -0700

    remove dead code

--- lib/rummager/util.rb
+++ lib/rummager/util.rb
@@ -1,17 +1,3 @@
 module Rummager
 
-  def Rummager.fingerprint_obj ( source_object )
-    content = nil
-    if source_object.is_a?( String )
-      content = source_object
-      Digest::MD5.hexdigest( source_object )
-    elsif source_object.is_a?( Dir )
-      files = Dir["#{source_object.path}/**/*"].reject{ |f| File.directory?(f) }
-      content = files.map{|f| File.read(f)}.join
-    else
-      raise ArgumentError, "Unhandled argument: #{source_object.to_s} is not a String or Dir"
-    end
-    Digest::MD5.hexdigest(content)
-  end
-
 end # Rummager
