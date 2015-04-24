a43c08e082c1711f3bcb82f79a0421163155ac1f
Theodore Vaida <ted@kstechnologies.com>
2015-04-24 14:07:56 -0600

    add helper functions for git operations inside a container

--- lib/rummager/util.rb
+++ lib/rummager/util.rb
@@ -1,3 +1,39 @@
 module Rummager
 
+    def Rummager.cmd_gitmirror(filepath,giturl)
+        {
+            :cmd=> [ "/bin/bash","-c",
+            "if [[ -d #{filepath} ]]; then\n"                       \
+            "  /usr/bin/git --git-dir=#{filepath} fetch --all\n"    \
+            "else\n"                                                \
+            "  /usr/bin/git clone --mirror #{giturl} #{filepath}\n" \
+            "fi\n"],
+        }
+    end # mirror_or_update
+
+    def Rummager.cmd_gitupdate(filepath)
+        {
+            :cmd=> [ "/bin/bash","-c",
+            "/usr/bin/git --git-dir=#{filepath} fetch --all",
+            ],
+        }
+    end # cmd_gitupdate
+
+    def Rummager.cmd_gitclone(branch,srcpath,clonepath)
+        {
+            :cmd => ["/bin/sh","-c",
+            "/usr/bin/git clone --branch #{branch} #{srcpath} #{clonepath}"
+            ],
+        }
+    end # cmd_gitclone
+
+    def Rummager.cmd_gitcheckout(commithash,srcpath,clonepath)
+        {
+            :cmd => ["/bin/sh","-c",
+            "/usr/bin/git clone --no-checkout #{srcpath} #{clonepath}\n"  \
+            "/usr/bin/git --work-tree #{clonepath} --git-dir #{clonepath}/.git checkout #{commithash}\n"
+            ],
+        }
+    end # cmd_gitclone
+
 end # Rummager
